10916f81eed3d2ac0b0288c1eea4d08c24020ccf
Piero Martinez <martinezlimop@gmail.com>
2025-07-28 18:17:16 -0500

    Quinto cambio

--- Principal.java
+++ Principal.java
@@ -1,6 +1,6 @@
 public class Principal {
     public static void main(String[] args) {
         System.out.println("Hola mundo, soy Piero!");
-        System.out.println("¡Haré un tercer commit!");
+        System.out.println("¡Haré un quinto commit!");
     }
 }
