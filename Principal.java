5f4e744406858bcbfbc01f5e3a676ed3b77a742f
Piero Martinez <martinezlimop@gmail.com>
2025-07-28 19:57:12 -0500

    Ultimo commit del día

--- Principal.java
+++ Principal.java
@@ -2,5 +2,8 @@ public class Principal {
     public static void main(String[] args) {
         System.out.println("Hola mundo, soy Piero!");
         System.out.println("¡Haré un quinto commit!");
+        System.out.println("==========================================");
+        System.out.println("Estoy cansado, dejaré el curso hasta aqui.");
+        System.out.println("Mañana lo continuo.");
     }
 }
