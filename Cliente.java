8d902baba772739bfde65e9f83da2ed8c41095ac
Piero Martinez <martinezlimop@gmail.com>
2025-07-30 11:20:27 -0500

    modulo_facturacion_lista

---
+++ Cliente.java
@@ -0,0 +1,11 @@
+public class Cliente {
+    /* Atributos */
+    String nombre;
+    String dni;
+
+    /* Constructor con parámetros */
+    public Cliente(String nombre, String dni) {
+        this.nombre = nombre;
+        this.dni = dni;
+    }
+}
